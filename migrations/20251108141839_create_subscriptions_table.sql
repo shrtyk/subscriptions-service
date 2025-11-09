b98c5dfb96e93485c65e3db4e46aaf0fa1183a3c
shortyk <shortykofficial@gmail.com>
2025-11-09 20:27:44 +0300

    feat: add initial migration for subscriptions table

---
+++ migrations/20251108141839_create_subscriptions_table.sql
@@ -0,0 +1,23 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE subscriptions (
+  id UUID PRIMARY KEY DEFAULT uuidv7 (),
+  service_name VARCHAR(255) NOT NULL,
+  monthly_cost DECIMAL(10, 2) NOT NULL,
+  user_id UUID NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_subscriptions_user_id ON subscriptions (user_id);
+
+CREATE INDEX idx_subscriptions_service_name ON subscriptions (service_name);
+
+-- +goose StatementEnd
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS subscriptions;
+
+-- +goose StatementEnd
