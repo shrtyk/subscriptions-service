05ecb260efd4be622e9f701c600d133b6a1bc173
shortyk <shortykofficial@gmail.com>
2025-11-11 22:01:42 +0300

    feat: add http helpers
    request parsers and reponse writers

---
+++ internal/api/http/request_parser.go
@@ -0,0 +1,79 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
+	"github.com/tomasen/realip"
+)
+
+func ReadJSON[T any](w http.ResponseWriter, r *http.Request, dst T) error {
+	maxBytes := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(dst); err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+		var maxBytesError *http.MaxBytesError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		case errors.As(err, &invalidUnmarshalError):
+			// Shouldn't occur at all. Only possible if wrong value passed as dst
+			panic(err)
+		default:
+			return err
+		}
+	}
+
+	if err := dec.Decode(&struct{}{}); err != nil {
+		if !errors.Is(err, io.EOF) {
+			return errors.New("body must contain a single JSON value")
+		}
+	}
+
+	return nil
+}
+
+func SubIdParam(r *http.Request) (*uuid.UUID, error) {
+	strId := chi.URLParam(r, "id")
+	if err := uuid.Validate(strId); err != nil {
+		return nil, err
+	}
+
+	subId, err := uuid.Parse(strId)
+	if err != nil {
+		return nil, err
+	}
+
+	return &subId, nil
+}
+
+func UserAgentAndIP(r *http.Request) (string, string) {
+	return r.UserAgent(), realip.FromRequest(r)
+}
---
+++ internal/api/http/response_writer.go
@@ -0,0 +1,80 @@
+package http
+
+import (
+	"bytes"
+	"encoding/json"
+	"maps"
+	"net/http"
+
+	"github.com/shrtyk/subscriptions-service/pkg/log"
+)
+
+type customResponseWriter struct {
+	http.ResponseWriter
+	statusCode  int
+	wroteHeader bool
+}
+
+func (w *customResponseWriter) WriteHeader(statusCode int) {
+	if w.wroteHeader {
+		return
+	}
+
+	w.statusCode = statusCode
+	w.wroteHeader = true
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *customResponseWriter) Write(b []byte) (int, error) {
+	if !w.wroteHeader {
+		w.WriteHeader(http.StatusOK)
+	}
+	return w.ResponseWriter.Write(b)
+}
+
+func WriteHTTPError(w http.ResponseWriter, r *http.Request, e *HttpError) {
+	l := log.FromCtx(r.Context())
+	if e.DTOErr.Code >= 500 {
+		l.Error("Server error", log.WithErr(e))
+	} else {
+		l.Info("Client error", log.WithErr(e))
+	}
+
+	err := WriteJSON(
+		w,
+		e.DTOErr,
+		int(e.DTOErr.Code),
+		nil,
+	)
+	if err != nil {
+		l.Error("Failed to response with error", log.WithErr(err))
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func WriteJSON[T any](w http.ResponseWriter, data T, status int, headers http.Header) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Uncomment to add extra readability during manual testing:
+	//
+	b = append(b, '\n')
+	buf := bytes.Buffer{}
+	if err := json.Indent(&buf, b, "", "\t"); err != nil {
+		return err
+	}
+	b = buf.Bytes()
+	//
+
+	maps.Copy(w.Header(), headers)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	if _, err = w.Write(b); err != nil {
+		return err
+	}
+
+	return nil
+}
